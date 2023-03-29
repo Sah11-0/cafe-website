c73bdd7aaaf11b997854c508901f1d315e545835
Sah11-0 <125934632+Sah11-0@users.noreply.github.com>
2023-03-29 03:11:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector(".nav-menu")
+const navbarItems = document.querySelector('.navbar ul');
 const menuLunchElements = document.querySelectorAll('.menu-lunch');
 const menuCoffeeElements = document.querySelectorAll('.menu-coffee');
 const menuDessertElements = document.querySelectorAll('.menu-dessert');
 const menuLinks = document.querySelectorAll('.menu-links a');
 
 
-
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active")
 })
+
+hamburger.addEventListener('click', () => {
+  navbarItems.classList.toggle('show');
+});
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
 
 menuLunchElements.forEach((menuLunchElement) => {
   menuLunchElement.style.display = 'none';
